e60f4f58904d228ba45bc416eb27fe8f81b98a58
Toth Zoltan2018 <toth.tozso.zoltan@gmail.com>
2018-12-05 10:53:42 +0100

    Initial version for "Todo" app. No DB usage.

---
+++ WebApp/WebApp/Controllers/TodoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+
+    public class TodoController : Controller
+    {
+        public ActionResult ToDoList()
+        {
+            ViewBag.Message = "Ezt a " + nameof(TodoController) + "-bol irattam ki.";
+            return View(MyDb.Lista);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string name, bool isDone)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {//Ha van adat a parameterben
+                //adatok mentese es vissza az indexre
+                MyDb.Lista.Add(new TodoItem() { Name = name, Done = isDone });
+
+                return RedirectToAction("TodoList");
+            }
+            //todo: mivel a adat nem valid, itt kene a hibauzenettel valamit kezdeni
+            //(kiadni az ugyfel fele)
+            return View();
+        }
+    }
+}
---
+++ WebApp/WebApp/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//A Model mappan jobb klikkel adtuk hozza ezt az osztalyt
+namespace WebApp.Models
+{
+    public class TodoItem
+    {
+        public string Name { get; internal set; }
+        public bool Done { get; internal set; }
+    }
+}
